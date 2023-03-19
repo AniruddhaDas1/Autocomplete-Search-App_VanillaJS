c97bc12bb3cdd118f3fb320fededcee549baf47d
ani294 <aniruddha.294@gmail.com>
2023-03-19 18:30:44 +0530

    1.0.0 - Initial Release

---
+++ script.js
@@ -0,0 +1,49 @@
+const availableKeywords = [
+  "Cooking queries",
+  "Love-related queries",
+  "Coming-of-age queries",
+  "Difficult/technical queries",
+  "Health queries",
+  "how to tie a tie",
+  "how to kiss",
+  "how to get pregnant",
+  "how to lose weight",
+  "how to draw",
+  "how to make money",
+  "how to make pancakes",
+  "how to write a cover letter",
+  "how to make french toast",
+  "how to lose belly fat",
+  "Where to learn coding",
+  "where to learn JS",
+];
+
+const resultBox = document.querySelector(".result-box");
+const inputBox = document.getElementById("input-box");
+
+inputBox.onkeyup = function(){
+  let result = [];
+  let input = inputBox.value;
+  if(input.length){
+    result = availableKeywords.filter((keyword)=>{
+      return keyword.toLowerCase().includes(input.toLowerCase());
+    });
+  }
+  display(result);
+
+  if(!result.length){
+    resultBox.innerHTML = '';
+  }
+}
+
+function display(result){
+  const content = result.map((list) =>{
+    return "<li onclick=selectInput(this)>" + list + "</li>";
+  })
+  resultBox.innerHTML = "<ul>" + content.join("") + "</ul>"
+}
+
+function selectInput(list){
+  inputBox.value = list.innerHTML;
+  resultBox.innerHTML = '';
+}
